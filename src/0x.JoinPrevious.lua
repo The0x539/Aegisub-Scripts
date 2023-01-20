8d4e7b1458d03fffe181b28c68cd359b93a36a3e
garret <garret@airmail.cc>
2023-01-20 18:47:10 +0000

    Join Previous: Ignore commented lines
    I don't want it to affect commented lines, because that's not any use for timing. I don't want to remove the comments though.
    Now, if the previous line is commented, it goes to the line before it, and changes that instead.
    If that line's commented, it goes to the line before that, and so on.
    
    I tried to do it by just modifying subs[j] directly, but strangely, it didn't work.
    So, I create the `previous` variable from subs[j], modify it, and put it right back into subs[j].
    Obviously not ideal, but i tried the good thing and it didnt work so what am i supposed to do.
    
    This commit also bumps the version number.

--- src/0x.JoinPrevious.lua
+++ src/0x.JoinPrevious.lua
@@ -3,12 +3,16 @@ local tr = aegisub.gettext
 script_name = tr"Join previous"
 script_description = tr"Set previous line's end to selected line's start"
 script_author = "The0x539"
-script_version = "1"
+script_version = "2"
 
 local function join_previous(subs, sel, i)
-    local line = subs[i - 1]
-    line.end_time = subs[i].start_time
-    subs[i - 1] = line
+    local j = i - 1
+    while subs[j].comment do
+        j = j - 1
+    end
+    local previous = subs[j]
+    previous.end_time = subs[i].start_time
+    subs[j] = previous
     aegisub.set_undo_point(tr"join previous")
 end
 
